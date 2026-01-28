d012f34e915d42f7b5ff15cd4469a41ac0cd62a2
Anushka De <anushkad@stanford.edu>
2026-01-28 09:02:12 -0800

    all for the theme bro

--- components/header.tsx
+++ components/header.tsx
@@ -5,6 +5,7 @@ import { Menu, X } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 import { cn } from '@/lib/utils'
+import { ThemeToggleButton } from './ThemeToggleButton'
 
 const menuItems = [
     { name: 'Features', href: '#link' },
@@ -102,6 +103,7 @@ export const HeroHeader = () => {
                                         <span>Get Started</span>
                                     </Link>
                                 </Button>
+                                <ThemeToggleButton />
                             </div>
                         </div>
                     </div>
