7bdcea8ef27b017297e3c200830884131502f777
Anushka De <anushkad@stanford.edu>
2026-01-28 10:50:29 -0800

    logo

--- components/header.tsx
+++ components/header.tsx
@@ -8,10 +8,9 @@ import { cn } from '@/lib/utils'
 import { ThemeToggleButton } from './ThemeToggleButton'
 
 const menuItems = [
-    { name: 'Features', href: '#link' },
-    { name: 'Solution', href: '#link' },
-    { name: 'Pricing', href: '#link' },
-    { name: 'About', href: '#link' },
+    { name: 'Search', href: '#link' },
+    { name: 'Writeup', href: '#link' },
+
 ]
 
 export const HeroHeader = () => {
